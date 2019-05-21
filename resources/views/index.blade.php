6dff18e6d45d7ca37e0ab534deac3f5f659ad7b5
Victor Hamil Diaz <hamil151@hotmail.com>
2019-05-21 02:36:59 -0400

    Historia Dominicana

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@ and open the template in the editor......
 <html>
     <head>
         <meta charset="UTF-8">
-    <title>{{ session('Historia') }}</title>
+    <title>Historia Dominicana</title>
         <!--Css-->
         <link rel="stylesheet" type="text/css" href="css/Style.css">
         <link rel="stylesheet" type="text/css" href="css/modalLogin.css">
