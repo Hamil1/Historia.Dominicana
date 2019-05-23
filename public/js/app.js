7fffcd354d48ca8af3e8149b420dac9130555776
Victor Hamil Diaz <hamil151@hotmail.com>
2019-05-23 14:23:33 -0400

    google button

--- public/js/app.js
+++ public/js/app.js
@@ -64045,8 +64045,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var React__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(React__WEBPACK_IMPORTED_MODULE_0__);
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -64055,13 +64053,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
@@ -64071,9 +64071,23 @@ function (_Component) {
   _inherits(Loginb, _Component);
 
   function Loginb() {
+    var _getPrototypeOf2;
+
+    var _this;
+
     _classCallCheck(this, Loginb);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Loginb).apply(this, arguments));
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Loginb)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "googleLoginHandler", function () {
+      console.log("Esta funcionando el login de google");
+    });
+
+    return _this;
   }
 
   _createClass(Loginb, [{
@@ -64084,9 +64098,11 @@ function (_Component) {
       return React__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
         "data-toggle": "modal"
       }, React__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        id: "googleButton"
+        id: "googleButton",
+        onClick: this.googleLoginHandler
       }, React__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-        src: "/imagenes/googleButton.png"
+        src: "/imagenes/googleButton.png",
+        height: "20"
       }), "Sign in with Google"), React__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", (_React$createElement = {
         id: "iniciarSesion",
         "data-toggle": "modal",
--- resources/js/components/loginButton.js
+++ resources/js/components/loginButton.js
@@ -1,10 +1,14 @@
 import React ,{Component} from 'React';
 
 class Loginb extends Component{
+
+    googleLoginHandler = ()=>{
+        console.log(`Esta funcionando el login de google`);
+    }
     render(){
         return(
             <span data-toggle="modal">
-                <button id="googleButton"><img src="/imagenes/googleButton.png" />Sign in with Google</button>
+                <button id="googleButton" onClick={this.googleLoginHandler}><img src="/imagenes/googleButton.png" height="20"/>Sign in with Google</button>
                 <button id="iniciarSesion" data-toggle="modal" className="accederLogin cd-signin" data-toggle="tooltip" title="Iniciar sesión" data-placement="left">Iniciar sesión</button>
             </span>
         );
