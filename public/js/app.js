4e654bdcfadeb5ccc2893eaa4831634678bb79b1
Victor Hamil Diaz <hamil151@hotmail.com>
2019-05-23 14:27:14 -0400

    google login

--- public/js/app.js
+++ public/js/app.js
@@ -64084,7 +64084,21 @@ function (_Component) {
     _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Loginb)).call.apply(_getPrototypeOf2, [this].concat(args)));
 
     _defineProperty(_assertThisInitialized(_this), "googleLoginHandler", function () {
-      console.log("Esta funcionando el login de google");
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken; // The signed-in user info.
+
+        var user = result.user; // ...
+      })["catch"](function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message; // The email of the user's account used.
+
+        var email = error.email; // The firebase.auth.AuthCredential type that was used.
+
+        var credential = error.credential; // ...
+      });
     });
 
     return _this;
--- resources/js/components/loginButton.js
+++ resources/js/components/loginButton.js
@@ -2,8 +2,24 @@ import React ,{Component} from 'React';
 
 class Loginb extends Component{
 
-    googleLoginHandler = ()=>{
-        console.log(`Esta funcionando el login de google`);
+    googleLoginHandler = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(result => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            let token = result.credential.accessToken;
+            // The signed-in user info.
+            let user = result.user;
+            // ...
+          }).catch(error => {
+            // Handle Errors here.
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            // The email of the user's account used.
+            let email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            let credential = error.credential;
+            // ...
+          });
     }
     render(){
         return(
